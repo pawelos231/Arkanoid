1c5904e0582ae49850244c3309af6bee3552d3bb
pawelLinek <pawellinek3D@gmail.com>
2022-11-30 21:00:28 +0100

    added fetcher

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
 const piec: number = 5
+const fetchVars =async() =>{
+ await fetch("http://localhost:8081/handler").then((res: Response) => res.json()).then(data => console.log(data))
+}
+fetchVars()
