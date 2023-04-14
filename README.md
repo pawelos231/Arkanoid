cd4333adc8fabbba592400f044a978a5d6be172d
pawelLinek <pawellinek3D@gmail.com>
2023-04-15 00:20:03 +0200

    pushed

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Arkanoid
 
+<p align='center'>
+<br>
+<i><b>[🚧 Work in progress! 🚧]</b></i>
+</p>
+
 My implementation of arkanoid game with custom addons, game comes also with backend to manage users and stats about each game it will
--- src/constants/api/Urls.ts
+++ src/constants/api/Urls.ts
@@ -1,3 +1,4 @@
 export const GET_STATS_URL = "http://localhost:3002/getStats"
 export const SEND_STATS_ABOUT_GAME = "http://localhost:3002/postStats"
 export const FETCH_LEVELS = "http://localhost:3002/getLevelData"
+export const FETCH_IMAGES_FOR_SONGS = "http://localhost:3002/images"
